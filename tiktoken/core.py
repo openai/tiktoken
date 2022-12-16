4226a6c008e394bb9fe5de688b1849ca4a88b27f
Henrik Torget <henriktorget@gmail.com>
2022-12-16 22:55:30 +0100

    Remove duplicate words in docstring (#3)

--- tiktoken/core.py
+++ tiktoken/core.py
@@ -59,7 +59,7 @@ class Encoding:
     ) -> list[int]:
         """Encodes a string into tokens.
 
-        Special tokens are tokens are artificial tokens used to unlock capabilities from a model,
+        Special tokens are artificial tokens used to unlock capabilities from a model,
         such as fill-in-the-middle. So we want to be careful about accidentally encoding special
         tokens, since they can be used to trick a model into doing something we don't want it to do.
 
