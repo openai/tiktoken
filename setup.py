c98f8240ed5d63ebc086e1e7d42ca681b174c381
Jack Gerrits <jackgerrits95@gmail.com>
2023-02-10 14:34:53 -0500

    Fix python setup.py for package in workspace

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ setup(
         RustExtension(
             "tiktoken._tiktoken",
             binding=Binding.PyO3,
+            path="python/Cargo.toml",
             # Between our use of editable installs and wanting to use Rust for performance sensitive
             # code, it makes sense to just always use --release
             debug=False,
-            args=["-p", "python"],
         )
     ],
     package_data={"tiktoken": ["py.typed"]},
