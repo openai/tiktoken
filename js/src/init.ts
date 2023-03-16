e1c4313a35a32e7b586aee2094192c0457a2bfa0
Tat Dat Duong <david@duong.cz>
2023-03-16 02:44:43 +0100

    Fix issues with duplicate initialization

--- js/src/init.ts
+++ js/src/init.ts
@@ -1,11 +1,13 @@
 // @ts-expect-error
 import * as imports from "./tiktoken_bg";
 
+let isInitialized = false;
 export async function init(
   callback: (
     imports: WebAssembly.Imports
   ) => Promise<WebAssembly.Instance | WebAssembly.WebAssemblyInstantiatedSource>
 ): Promise<void> {
+  if (isInitialized) return imports;
   const result = await callback({ "./tiktoken_bg.js": imports });
   const instance =
     "instance" in result && result.instance instanceof WebAssembly.Instance
@@ -15,6 +17,7 @@ export async function init(
       : null;
   if (instance == null) throw new Error("Missing instance");
   imports.__wbg_set_wasm(instance.exports);
+  isInitialized = true;
   return imports;
 }
 
