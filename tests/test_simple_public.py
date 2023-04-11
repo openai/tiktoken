82f27fdb1f3ecad94ed5ffa38f1e9a56d003836c
Ramseths <jesus.ramseths@gmail.com>
2023-04-10 20:00:45 -0600

    Refactored 'test_simple()'. Refactored test_encoding_for_model() using test cases list.

--- tests/test_simple_public.py
+++ tests/test_simple_public.py
@@ -4,33 +4,34 @@ import sys
 import tiktoken
 
 
-def test_simple():
-    # Note that there are more actual tests, they're just not currently public :-)
-    enc = tiktoken.get_encoding("gpt2")
-    assert enc.encode("hello world") == [31373, 995]
-    assert enc.decode([31373, 995]) == "hello world"
-    assert enc.encode("hello <|endoftext|>", allowed_special="all") == [31373, 220, 50256]
+def test_simple_encoding():
+    test_cases = [
+        ("gpt2", [[31373, 995], [31373, 220, 50256], [15339, 1917], [15339, 220, 100257]]),
+        ("cl100k_base", [[15339, 1917], [15339, 220, 100257]])
+    ]
 
-    enc = tiktoken.get_encoding("cl100k_base")
-    assert enc.encode("hello world") == [15339, 1917]
-    assert enc.decode([15339, 1917]) == "hello world"
-    assert enc.encode("hello <|endoftext|>", allowed_special="all") == [15339, 220, 100257]
-
-    for enc_name in tiktoken.list_encoding_names():
+    for enc_name, cases in test_cases:
         enc = tiktoken.get_encoding(enc_name)
+
+        assert enc.encode("hello world") == cases[0]
+        assert enc.decode(cases[0]) == "hello world"
+        assert enc.encode("hello ", allowed_special="all") == cases[1]
+
         for token in range(10_000):
             assert enc.encode_single_token(enc.decode_single_token_bytes(token)) == token
 
 
 def test_encoding_for_model():
-    enc = tiktoken.encoding_for_model("gpt2")
-    assert enc.name == "gpt2"
-    enc = tiktoken.encoding_for_model("text-davinci-003")
-    assert enc.name == "p50k_base"
-    enc = tiktoken.encoding_for_model("text-davinci-edit-001")
-    assert enc.name == "p50k_edit"
-    enc = tiktoken.encoding_for_model("gpt-3.5-turbo-0301")
-    assert enc.name == "cl100k_base"
+    test_cases = [
+        ("gpt2", "gpt2"),
+        ("text-davinci-003", "p50k_base"),
+        ("text-davinci-edit-001", "p50k_edit"),
+        ("gpt-3.5-turbo-0301", "cl100k_base")
+    ]
+
+    for model_name, expected_enc_name in test_cases:
+        enc = tiktoken.encoding_for_model(model_name)
+        assert enc.name == expected_enc_name
 
 
 def test_optional_blobfile_dependency():
@@ -40,3 +41,9 @@ import sys
 assert "blobfile" not in sys.modules
 """
     subprocess.check_call([sys.executable, "-c", prog])
+
+
+if __name__ == "__main__":
+    test_simple_encoding()
+    test_encoding_for_model()
+    test_optional_blobfile_dependency()
