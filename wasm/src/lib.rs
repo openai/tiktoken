5a7be3cb5b9820a86ca69a58acfbae1d67dca32f
Daewoon Kim <me@kdw.kr>
2023-06-16 10:52:22 +0900

    feat: Add missing models

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -384,8 +384,12 @@ export type TiktokenModel =
     | "gpt-4-0314"
     | "gpt-4-32k"
     | "gpt-4-32k-0314"
+    | "gpt-4-32k-0613"
     | "gpt-3.5-turbo"
-    | "gpt-3.5-turbo-0301";
+    | "gpt-3.5-turbo-0301"
+    | "gpt-3.5-turbo-0613"
+    | "gpt-3.5-turbo-16k"
+    | "gpt-3.5-turbo-16k-0613";
 
 /**
  * @param {TiktokenModel} encoding
@@ -439,6 +443,7 @@ pub fn encoding_for_model(
         "gpt-3.5-turbo-16k-0613" => Ok("cl100k_base"),
         "gpt-4" => Ok("cl100k_base"),
         "gpt-4-0314" => Ok("cl100k_base"),
+        "gpt-4-0613" => Ok("cl100k_base"),
         "gpt-4-32k" => Ok("cl100k_base"),
         "gpt-4-32k-0314" => Ok("cl100k_base"),
         "gpt-4-32k-0613" => Ok("cl100k_base"),
