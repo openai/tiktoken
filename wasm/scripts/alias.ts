4491997c6d7a6f5d38331ed7d99a755350a1e76b
Tat Dat Duong <david@duong.cz>
2024-01-27 23:35:35 -0800

    Backport changes to @dqbd/tiktoken

---
+++ wasm/scripts/alias.ts
@@ -0,0 +1,20 @@
+import * as fs from "node:fs/promises";
+import * as path from "node:path";
+
+async function createAlias() {
+  const srcDist = path.resolve(__dirname, "../dist");
+  const targetDist = path.resolve(__dirname, "../alias/dist");
+
+  await fs.cp(srcDist, targetDist, { recursive: true });
+
+  const pkgPath = path.resolve(srcDist, "package.json");
+
+  const pkg = JSON.parse(await fs.readFile(pkgPath, { encoding: "utf-8" }));
+  pkg["name"] = "@dqbd/tiktoken";
+
+  await fs.writeFile(pkgPath, JSON.stringify(pkg, null, 2), {
+    encoding: "utf-8",
+  });
+}
+
+createAlias();
