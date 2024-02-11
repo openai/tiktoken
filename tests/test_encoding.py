58cf8f69f40ef77c64199275e8f483a7e153e2a8
Lőrinc <lorinc.pap@gmail.com>
2024-02-11 22:48:53 +0100

    Add possessive quantifiers to legacy encodings as well

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -11,6 +11,7 @@ import tiktoken
 from .test_helpers import ENCODING_FACTORIES, MAX_EXAMPLES
 
 
+@pytest.mark.skip(reason="Takes a really long time to finish, but was added to reproduce a crash.")
 @pytest.mark.parametrize("make_enc", ENCODING_FACTORIES)
 def test_extremely_big_encoding(make_enc: Callable[[], tiktoken.Encoding]):
     enc = make_enc()
