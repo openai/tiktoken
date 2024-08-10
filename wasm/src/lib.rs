26ec6cc78cf9cefe4f3dbcac1c291f5017637ba6
Marco Barreto <63501911+mbarretol@users.noreply.github.com>
2024-08-10 11:33:12 +0200

    Update wasm/src/lib.rs
    Co-authored-by: huytool157 <97813378+huytool157@users.noreply.github.com>

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -420,6 +420,7 @@ export type TiktokenModel =
     | "gpt-4-vision-preview"
     | "gpt-4o"
     | "gpt-4o-2024-05-13"
+    | "gpt-4o-2024-08-06"
     | "gpt-4o-mini-2024-07-18"
     | "gpt-4o-mini"
 
